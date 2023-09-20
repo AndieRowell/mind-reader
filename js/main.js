435b1de01b564a903099f20f8cc1eacb9a85a284
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-09-20 15:43:50 -0400

    adding pseudocode to js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// pseudocode:
+// i have my html and elements
+// my ui has been created?
+// create json data for the views
+// need an event listener / click handler
+// obtain the json data
+//      using fetch? - callback function?
+//      json data is received as a string - need to convert to a js object
+//          parse method?
+// make a new variable that can hold the new html data for each view
+// loop through objects that represent the information/state?
+//      with their corresponding html elements
+// could increment the index of the information stored in arrays to change content displayed
+// contents of the objects will add to that new variable
+// new html view would be updated and rendered by manipulating the dom?
+//      update using document.getelementbyid?
