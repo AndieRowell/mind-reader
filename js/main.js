a10cb2c4cc2a5f0663180d13049bafdafe6b869a
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-09-21 14:37:42 -0400

    starting js

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@
             "go-button-content": //svg for the refresh icon
         }
     ]
-
+*/
 
 
 // HOW TO LOAD INITIALLY?
@@ -113,6 +113,42 @@
 // .textcontent - to populate text into the html elements...
 // - look into populating the refresh icon/svg
 // html - initialing variables and getElementById
+// including a scroll on view #4? - bootstrap?
 
 
-//button test
+
+    let views = [
+        {
+            "main-text": "I can read your mind",
+            "go-button-content": "GO"
+        },
+        {
+            "main-text": "Pick a number from 01 - 99",
+            "nav-button-text": "NEXT",
+            "sub-text": "when you have your number click next",
+            "go-button-content": //svg???? for the refresh icon?
+        },
+        {
+            "main-text": "Add both digits together to get a new number",
+            "nav-button-text": "NEXT",
+            "sub-text": "Ex: 14 is 1 + 4 = 5 click next to proceed",
+            "go-button-content": //svg for the refresh icon
+        },
+        {
+            "main-text": "Subtract your new number from the original number",
+            "nav-button-text": "NEXT",
+            "sub-text": "Ex: 14 - 5 = 9 click next to proceed",
+            "go-button-content": //svg for the refresh icon
+        },
+        {
+            "main-text": "0 - &, 1 - #, 2 - @, 1 - !, 3 - ~, 4 - $, 5 - %, 6 - ^, 7 - *, 8 - +, 9 - ?", // this is where i need to add a way that randomizes this specific string
+            "nav-button-text": "NEXT",
+            "sub-text": "Find your new number. Note the symbol beside the number",
+            "go-button-content": //svg for the refresh icon
+        },
+        {
+            "main-text": "?", //this would have to be connected to the randomization of the previous symbols string - whatever the new symbol for 9 is should replace
+            "sub-text": "Your symbol is ?", //also needs to connect to the randomize
+            "go-button-content": //svg for the refresh icon
+        }
+    ]
